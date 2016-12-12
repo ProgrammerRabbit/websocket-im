22b96ffcc2ef2e50995c3cfda51e41fce2361ed1
杨文 <rabbit_da@outlook.com>
2016-12-12 22:18:36 +0800

    Account CRUD

--- src/main/java/com/github/programmerrabbit/dao/AccountDao.java
+++ src/main/java/com/github/programmerrabbit/dao/AccountDao.java
@@ -1,11 +1,26 @@
 package com.github.programmerrabbit.dao;
 
 import com.github.programmerrabbit.entity.Account;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * Created by Rabbit on 2016/12/12.
  */
 public interface AccountDao {
-    Account getAccountByUsername(@Param("username") String username);
+    void insert(@Param("entity") Account entity);
+
+    @Select("SELECT * FROM Account WHERE id = #{id}")
+    Account getById(@Param("id") int id);
+
+    @Select("SELECT * FROM Account WHERE #{fieldName} = #{fieldValue}")
+    List<Account> getByField(@Param("fieldName") String filedName, @Param("fieldValue") Object fieldValue);
+
+    void updateById(@Param("id") int id, @Param("entity") Account entity);
+
+    @Delete("DELETE FROM Account WHERE id = #{id}")
+    void deleteById(@Param("id") int id);
 }
--- src/test/java/com/github/programmerrabbit/dao/AccountDaoTest.java
+++ src/test/java/com/github/programmerrabbit/dao/AccountDaoTest.java
@@ -1,10 +1,11 @@
 package com.github.programmerrabbit.dao;
 
 import com.github.programmerrabbit.BaseTest;
+import com.github.programmerrabbit.entity.Account;
 import org.junit.Test;
-import org.springframework.util.Assert;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by Rabbit on 2016/12/12.
@@ -14,8 +15,38 @@ public class AccountDaoTest extends BaseTest {
     private AccountDao accountDao;
 
     @Test
-    public void getAccountByUsername() throws Exception {
-        Assert.isNull(accountDao.getAccountByUsername("rabbit"));
+    public void insert() throws Exception {
+        Account account = new Account();
+        account.setUsername("rabbit");
+        account.setPassword("rabbit");
+        accountDao.insert(account);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        Account account = accountDao.getById(5);
+        System.out.println(account.toString());
+    }
+
+    @Test
+    public void getByField() throws Exception {
+        List<Account> accountList = accountDao.getByField("useranem", "rabbit");
+        for (Account account : accountList) {
+            System.out.println(account.toString());
+        }
+    }
+
+    @Test
+    public void updateById() throws Exception {
+        Account account = new Account();
+        account.setUsername("rabbit");
+        account.setPassword("tibbar");
+        accountDao.updateById(5, account);
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+        accountDao.deleteById(5);
     }
 
 }
